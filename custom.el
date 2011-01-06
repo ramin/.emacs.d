ed5198046f9bd568932c0a21e9ca3197e8006922
Ramin Keene <raminkeene@mac.com>
2011-01-06 08:03:49 -0800

    new color theme, speedbar, some rspec yasnippets, default rvm

--- custom.el
+++ custom.el
@@ -4,8 +4,14 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(espresso-indent-level 4 t)
+ '(fill-column 85)
  '(javascript-auto-indent-flag nil)
- '(show-paren-mode t))
+ '(show-paren-mode t)
+ '(speedbar-show-unknown-files t)
+ '(speedbar-use-images nil)
+ '(sr-speedbar-max-width 30)
+ '(sr-speedbar-right-side nil)
+ '(sr-speedbar-width-x 30))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
